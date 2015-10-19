3db2a98cba6cc89afaaf8702760d0cd39e83a4a4
Realhedin <korwork87@yahoo.com>
2015-10-19 19:42:58 +0300

    SynchronousQueue example.

---
+++ src/main/java/javaUtilConcurrentPackage/synchronousQueue/SynchronousQueueExample.java
@@ -0,0 +1,31 @@
+package javaUtilConcurrentPackage.synchronousQueue;
+
+import javaUtilConcurrentPackage.commonPart.Consumer;
+import javaUtilConcurrentPackage.commonPart.Producer;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * Simple example of using with SynchronousQueue implementation.
+ * Can only contain a single element internally!
+ * Producer is blocked if queue.size() > 1.
+ *
+ * @author dkorolev
+ *         Date: 19.10.15
+ *         Time: 20:39
+ */
+public class SynchronousQueueExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        BlockingQueue<String> blockingQueue = new SynchronousQueue<>();
+
+        Producer producer = new Producer(blockingQueue);
+        Consumer consumer = new Consumer(blockingQueue);
+
+        new Thread(producer).start();
+        new Thread(consumer).start();
+
+        Thread.sleep(3000);
+    }
+}
